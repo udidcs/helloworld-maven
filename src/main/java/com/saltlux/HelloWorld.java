9f44b327ae095180af92b25104fed363713a17b1
Byun JH <Byun JH@DESKTOP-T712Q22>
2021-03-09 08:40:09 +0900

    message

--- src/main/java/com/saltlux/HelloWorld.java
+++ src/main/java/com/saltlux/HelloWorld.java
@@ -4,6 +4,8 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("hello world");
 		System.out.println("hello world");
+		System.out.println("hello world");
+		System.out.println("hello world");
 		
 	}
 }
